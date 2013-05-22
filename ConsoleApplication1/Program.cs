20dd209d299be2626ea1a1384745efbfc7024f84
andre vermeulen <andre.vermeulen@gmail.com>
2013-05-23 00:55:42 +0100

    Added bootstrap support and reworked the screen layout.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Grandpree.Test;
 using Raven.Abstractions.Data;
 using Raven.Client;
 using Raven.Client.Document;
